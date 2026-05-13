4fdd10983105137ec82ad1faa69e66087ab86112
Abhishek Jha <abhishekjha@192.168.101.14>
2026-05-13 18:21:28 +0545

    establishing database connection

---
+++ src/index.js
@@ -0,0 +1,31 @@
+
+//require("dotenv").config({path: "./env"});
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+
+dotenv.config({ path: "./env" });
+
+
+connectDB();
+
+
+
+
+/*
+export const connectDB = async () => {
+    try {
+        await mongoose.connect('${process.env.MONGO_URI}/${DB_NAME}');
+        console.log("Connected to MongoDB");
+        app.on("error", (error) => {
+            console.error("MongoDB connection error:", error);
+        });
+
+        app.listen(process.env.PORT, () => {
+            console.log(`Server is running on port ${process.env.PORT}`);
+        }
+    } catch (error) {
+        console.error("Error connecting to MongoDB:", error);
+        process.exit(1);
+    }
+}
+*/
